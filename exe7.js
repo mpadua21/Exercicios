3dfa63bd38a88d069c31d62c34f78e6561bdea3a
Matheus Padua <matheuspo21@hotmail.com>
2020-08-24 21:55:12 -0300

    Exercicio 7 Realizado

---
+++ exe7.js
@@ -0,0 +1,21 @@
+chefe()
+
+function chefe(){
+    exe7()
+}
+
+
+function exe7(){
+    var numero = Number(prompt(`Informe um Número`))
+var soma = 0
+var conta = 0
+
+    do{
+        soma = soma + numero // acumulador
+        conta++ //soma1
+        var numero = Number(prompt(`Informe um Número! Digite um número negativo para encerrar`))
+        var media = soma/i
+    }
+    while(numero>=0)
+    console.log(`A média dos números informados é de ${media}`)
+}
